1d622de9be4bc376782b1a1780baa0147118f369
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2014-06-23 12:14:53 -0500

    Initial commit.

--- Gemfile
+++ Gemfile
@@ -2,10 +2,17 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
-gem "middleman", "~>3.2.0"
+gem "sass", "~> 3.3.0"
+#gem "middleman", "~> 3.3.0"
+gem "middleman-core", ">= 3.3.0"
+gem "middleman", ">= 3.3.0"
 
 # Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-livereload", ">= 3.1.0"
+gem 'compass', '~> 1.0.0.alpha.19'
 
 # For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem "wdm", ">= 0.1.0", :platforms => [:mswin, :mingw]
+
+gem "middleman-deploy"
+gem "middleman-gh-pages"
---
+++ source/js/main.js
@@ -0,0 +1,27 @@
+//= require jquery
+
+var showText = function (target, message, index, interval) {
+  if (index < message.length) {
+    console.log("Executing!");
+    $(target).before(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  } else {
+    console.log("Done!");
+  }
+}
+
+$(function () {
+  /* Responsive Navigation */
+  //var nav = responsiveNav(".nav-collapse");
+  /* Get current page URL */
+  var currentPage = window.location.pathname;
+  console.log(currentPage);
+  if (currentPage == '/') {
+    showText(".cursor", "Hello, World! ", 0, 200);
+  } else if (currentPage == '/about') {
+    showText(".cursor", "Who Am I? ", 0, 175);
+  }
+});
+
+
+
